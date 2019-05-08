e2407da8187d54a140eb91fbb3ab6af4b49c942a
Arman <arman.vaziri1@gmail.com>
2019-05-08 03:26:23 -0700

    name and address populate search

--- PPiOSProject/PP-Project/LocationSearchTable.swift
+++ PPiOSProject/PP-Project/LocationSearchTable.swift
@@ -16,6 +16,30 @@ class LocationSearchTable: UITableViewController {
     var matchingItems:[MKMapItem] = []
     var mapView: MKMapView? = nil
     
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
+    }
+    
 }
 
 extension LocationSearchTable : UISearchResultsUpdating {
@@ -44,9 +68,9 @@ extension LocationSearchTable {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        let selectedIem = matchingItems[indexPath.row].placemark
-        cell.textLabel?.text = selectedIem.name
-        cell.detailTextLabel?.text = ""
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
         return cell
     }
 }
